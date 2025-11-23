3273093c448a429e9556f538a6b6c728889f91d0
kadkim044 <kadkim044@gmail.com>
2025-11-23 23:15:41 +0100

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -151,6 +151,19 @@ function moveBirdA(){
     velocityY=-6;
     wingSound.currentTime=0;
     wingSound.play()
+    if(bgSound.paused){
+        bgSound.currentTime=0;
+        bgSound.play();
+    }
+    bgSound.play();
+
+    if (gameOver) {
+        bird.y = birdY;
+        pipes = [];
+        score = 0;
+        gameOver = false;
+        update()
+    }
 }
 function checkColision(a,b){
     return a.x<b.x+b.width&&
